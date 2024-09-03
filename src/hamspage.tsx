e20e2b0971d82e304763b859795c603cae79d655
ceecosmonew <pagefamex@gmail.com>
2024-09-03 21:44:58 +0100

    Update hamspage.tsx

--- src/hamspage.tsx
+++ src/hamspage.tsx
@@ -1,3 +1,8 @@
+// import binanceLogo from './images/binance-logo.png';
+import Mine from './icons/Mine';
+import Friends from './icons/Friends';
+import Coins from './icons/Coins';
+import { hamsterCoin } from './images';
 
 function hamspage() {
   return (
@@ -18,6 +23,34 @@ function hamspage() {
       <a href="https://api.whatsapp.com/send?text=https://t.me/HamsPay_bot" target="_blank">Whatsapp</a>
     
     </div>
+
+
+<div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 w-[calc(100%-2rem)] max-w-xl bg-[#272a2f] flex justify-around items-center z-50 rounded-3xl text-xs">
+
+<div className="text-center text-[#85827d] w-1/5 bg-[#1c1f24] m-1 p-2 rounded 2xl">
+
+  {/* <img src={binanceLogo} alt="Exchange" className="w-8 h-8 mx-auto" /> */}
+  <p className="mt-1"><a href="/">Home</a></p>
+</div>
+
+
+<div className="text-center text-[#85827d] w-1/5"><Mine className="w-8 h-8 mx-auto"/>
+<p className="mt-1">Mine</p>
+</div>
+<div className="text-center text-[#85827d] w-1/5"><Friends className="w-8 h-8 mx-auto"/>
+<p className="mt-1"><a href="/hamspage">Friends</a></p>
+</div>
+<div className="text-center text-[#85827d] w-1/5"><Coins className="w-8 h-8 mx-auto"/>
+<p className="mt-1">Earn</p>
+</div>
+<div className="text-center text-[#85827d] w-1/5">
+  <img src={hamsterCoin} className="w-8 h-8 mx-auto"/>
+<p className="mt-1">Airdrop</p>
+</div>
+
+</div>
+
+      
     </div>
   )
 }
